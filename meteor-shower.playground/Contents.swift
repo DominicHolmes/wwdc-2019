317eccea59d35d9c0b407e6bf29820f152f43818
dominic <hello@dom.voyage>
2019-03-17 15:57:26 +0000

    Added meteor logic into starryNight.

--- meteor-shower.playground/Contents.swift
+++ meteor-shower.playground/Contents.swift
@@ -2,15 +2,6 @@ import UIKit
 import PlaygroundSupport
 import AVFoundation
 
-extension CGPoint {
-    func angle(to comparisonPoint: CGPoint) -> CGFloat {
-        let originX = comparisonPoint.x - self.x
-        let originY = comparisonPoint.y - self.y
-        let bearingRadians = atan2f(Float(originY), Float(originX))
-        return CGFloat(bearingRadians)
-    }
-}
-
 class MeteorViewController : UIViewController {
     override func loadView() {
         let view = UIView()
@@ -44,12 +35,11 @@ class MeteorViewController : UIViewController {
     }
     
     @objc func spawnMeteors() {
-        listenVolumeButton()
         
-        /*for _ in 0...300 {
+        for _ in 0...10 {
             let xoff = CGFloat.random(in: -200.0...200.0)
             let yoff = CGFloat.random(in: -200.0...200.0)
-            let timeOff = Double.random(in: 0.0...2.0)
+            let timeOff = Double.random(in: 0.0...10.0)
             let radius = CGFloat.random(in: 150...400)
             
             let params = MeteorParams(radius: radius,
@@ -58,16 +48,16 @@ class MeteorViewController : UIViewController {
                                       origin: CGPoint(x: 300 + xoff, y: 300 + yoff))
             
             DispatchQueue.main.asyncAfter(deadline: .now() + timeOff) {
-                self.addMeteor(with: params)
+                self.spawnMeteor(with: params)
             }
-        }*/
+        }
         let params = MeteorParams(radius: 300,
                                   startAngleFuzz: 0.0,
                                   endAngleFuzz: 0.0,
                                   origin: CGPoint(x: 300, y: 300))
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.addMeteor(with: params)
+            self.spawnMeteor(with: params)
         }
     }
     
@@ -78,9 +68,7 @@ class MeteorViewController : UIViewController {
         let origin: CGPoint
     }
     
-    
-    
-    func addMeteor(with params: MeteorParams) {
+    func spawnMeteor(with params: MeteorParams) {
         let r: CGFloat = params.radius
         let pi = CGFloat.pi
         let origin = params.origin
@@ -96,35 +84,30 @@ class MeteorViewController : UIViewController {
         path.move(to: start)
         path.addArc(withCenter: CGPoint(x: r, y: r), radius: r, startAngle: startAngleMult * pi, endAngle: endAngleMult * pi, clockwise: false)
         
-        let meteor = CAShapeLayer()
-        meteor.path = path.cgPath
-        meteor.frame = CGRect(x: 0, y: 0, width: r * 2, height: r * 2)
-        meteor.lineCap = .round
-        meteor.strokeColor = UIColor.white.cgColor
-        meteor.fillColor = UIColor.clear.cgColor
-        meteor.lineWidth = 1.0
-        meteor.strokeStart = 0.0
-        meteor.strokeEnd = 1.0
-        meteor.opacity = 0.0
+        let meteorMask = CAShapeLayer()
+        meteorMask.path = path.cgPath
+        meteorMask.frame = CGRect(x: 0, y: 0, width: r * 2, height: r * 2)
+        meteorMask.lineCap = .round
+        meteorMask.strokeColor = UIColor.white.cgColor
+        meteorMask.fillColor = UIColor.clear.cgColor
+        meteorMask.lineWidth = 1.0
+        meteorMask.strokeStart = 0.0
+        meteorMask.strokeEnd = 1.0
+        meteorMask.opacity = 0.0
         
         // Create and add the gradient layer
-        let gradient = CAGradientLayer()
-        gradient.frame = CGRect(x: origin.x - r, y: origin.y - r, width: r * 2, height: r * 2)
-        print(gradient.frame)
-        gradient.colors = [UIColor.clear.cgColor, UIColor.white.cgColor]
+        let meteorGradient = CAGradientLayer()
+        meteorGradient.frame = CGRect(x: origin.x - r, y: origin.y - r, width: r * 2, height: r * 2)
+        meteorGradient.colors = [UIColor.clear.cgColor, UIColor.white.cgColor]
 
         let gradientTail = CGPoint(x: start.x / r,
                                    y: start.y / r)
         let gradientHead = CGPoint(x: end.x / r,
                                    y: end.y / r)
-        gradient.startPoint = gradientTail
-        gradient.endPoint = gradientHead
-        gradient.locations = [0.0, 1.0]
-        gradient.mask = meteor
-        
-        view.layer.addSublayer(gradient)
-        
-        
+        meteorGradient.startPoint = gradientTail
+        meteorGradient.endPoint = gradientHead
+        meteorGradient.locations = [0.0, 1.0]
+        meteorGradient.mask = meteorMask
         
         // Create stroke animations
         let strokeStartAnimation = CABasicAnimation(
@@ -145,9 +128,13 @@ class MeteorViewController : UIViewController {
         opacityAnimation.keyTimes = [0.0, 0.5, 1.0]
         opacityAnimation.duration = 1.3
         
-        meteor.add(strokeStartAnimation, forKey: "meteorAnimation")
-        meteor.add(strokeEndAnimation, forKey: "meteorEndAnimation")
-        meteor.add(opacityAnimation, forKey: "meteorOpacityAnimation")
+        // Add the meteor gradient to the view
+        view.layer.addSublayer(meteorGradient)
+        
+        // Animate the meteor mask
+        meteorMask.add(strokeStartAnimation, forKey: "meteorAnimation")
+        meteorMask.add(strokeEndAnimation, forKey: "meteorEndAnimation")
+        meteorMask.add(opacityAnimation, forKey: "meteorOpacityAnimation")
     }
 }
 // Present the view controller in the Live View window
